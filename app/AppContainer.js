dfcdcca6c22227348acf984093e85887bb1a4c6b
Pascal Bugnion <pascal@bugnion.org>
2018-07-16 17:20:56 +0100

    Avoid magic strings

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -2,12 +2,13 @@
 import { connect } from 'react-redux'
 
 import App from './app'
+import { APP_START } from './actions'
 
 const mapStateToProps = state => {return {}}
 
 const mapDispatchToProps = dispatch => {
     return {
-	onAppStart: () => { dispatch({ type: 'APP_START' }) }
+	onAppStart: () => { dispatch({ type: APP_START }) }
     }
 }
 
