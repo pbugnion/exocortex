0f2b24e4e84fc05259535bc015036b51c23d1501
Pascal Bugnion <pascal@bugnion.org>
2018-07-16 06:51:59 +0100

    Split structure into directories per component

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './store'
 
 // import MarkdownViewer from './MarkdownViewer'
-import PostSelector from './PostSelector'
+import PostSelector from './contents/PostSelector'
 
 import 'photonkit/dist/css/photon.css'
 
