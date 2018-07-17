1560a20a782adaaa61d7a7229ee2bdff35359d10
Pascal Bugnion <pascal@bugnion.org>
2018-07-17 16:16:37 +0100

    Read tags from metadata

--- app/actions.js
+++ app/actions.js
@@ -2,6 +2,7 @@ import { takeEvery, all, put, cps } from 'redux-saga/effects'
 
 import fs from 'fs'
 import path from 'path'
+import yaml from 'js-yaml'
 
 import { MarkdownParser } from './services/markdown'
 
@@ -9,6 +10,7 @@ export const APP_START = 'APP_START'
 export const RECEIVED_POST_LIST = 'RECEIVED_POST_LIST'
 export const RECEIVED_POST_CONTENTS = 'RECEIVED_POST_CONTENTS'
 export const RECEIVED_POST_AST = 'RECEIVED_POST_AST'
+export const RECEIVED_POST_ATTRIBUTES = 'RECEIVED_POST_ATTRIBUTES'
 
 function* watchAppStartup() {
     yield takeEvery(APP_START, getPostList)
@@ -25,6 +27,15 @@ function* getPostList() {
 	yield put({ type: RECEIVED_POST_CONTENTS, filePath, contents })
 	const ast = MarkdownParser.parse(contents)
 	yield put({ type: RECEIVED_POST_AST, filePath, ast })
+	const frontMatterNode = ast.children.find(({ type }) => type === 'yaml')
+	if (typeof frontMatterNode !== 'undefined') {
+	    const frontMatter = frontMatterNode.value
+	    const loadedYaml = yaml.safeLoad(frontMatter)
+	    const tags = loadedYaml.tags ? loadedYaml.tags : []
+	    yield put({ type: RECEIVED_POST_ATTRIBUTES, filePath, tags })
+	} else {
+	    yield put({ type: RECEIVED_POST_ATTRIBUTES, filePath, tags: [] })
+	}
     }
 }
 
--- app/reducer/index.js
+++ app/reducer/index.js
@@ -4,7 +4,8 @@ import { combineReducers } from 'redux'
 import {
     RECEIVED_POST_LIST,
     RECEIVED_POST_CONTENTS,
-    RECEIVED_POST_AST
+    RECEIVED_POST_AST,
+    RECEIVED_POST_ATTRIBUTES
 } from '../actions'
 
 const initialPostState = {
@@ -29,6 +30,13 @@ function posts(state = initialPostState, action) {
 	const { filePath, ast } = action
 	const oldPost = state.posts[filePath]
 	const newPost = { ...oldPost, ast }
+	const newPosts = { ...state.posts, [filePath]: newPost }
+	return { ...state, posts: newPosts }
+    }
+    case RECEIVED_POST_ATTRIBUTES: {
+	const { filePath, tags } = action
+	const oldPost = state.posts[filePath]
+	const newPost = { ...oldPost, tags }
 	const newPosts = { ...state.posts, [filePath]: newPost }
 	return { ...state, posts: newPosts }
     }
