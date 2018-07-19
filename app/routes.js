0b1306085c981a94efcfd56c8534e1c72a267dbc
Pascal Bugnion <pascal@bugnion.org>
2018-07-19 06:40:49 +0100

    Rename PostViewContainer to PostPageContainer
    This will allow placing multiple components, including a navbar, on
    the page.

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,17 @@ import React from 'react'
 
 import { Route, Switch } from 'react-router'
 
-import ContentsPageContainer from './contents/ContentsPageContainer'
-import PostViewContainer from './postView/PostViewContainer'
+import ContentsPage from './contents/ContentsPageContainer'
+import PostPage from './posts/PostPageContainer'
 
 export default () => (
     <Switch>
-	<Route exact path="/" component={ContentsPageContainer} />
+	<Route exact path="/" component={ContentsPage} />
 	<Route
 	  path="/post" // Expect post path in query string
 	  render={({ location }) => {
 	      const query = new URLSearchParams(location.search)
-	      return <PostViewContainer postPath={query.get('path')} />
+	      return <PostPage postPath={query.get('path')} />
 	  }}
 	/>
     </Switch>
