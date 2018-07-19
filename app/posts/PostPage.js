d860355adbd352052e7a50f14b43595b1b68c41e
Pascal Bugnion <pascal@bugnion.org>
2018-07-19 08:00:12 +0100

    Split navbar into separate component

--- app/posts/PostPage.js
+++ app/posts/PostPage.js
@@ -1,23 +1,15 @@
 
 import React, { Component } from 'react'
 
+import PostNavbar from './PostNavbar'
 import PostView from './PostView'
 
 class PostPage extends Component {
     render() {
-	const { postPath, isLoaded, post } = this.props
+	const { postPath, isLoaded, post, onNavigateHome } = this.props
 	return (
 	    <div>
-	      <header className="toolbar toolbar-header toolbar-borderless">
-		<h1 className="title">{postPath}</h1>
-		<div className="toolbar-actions">
-		  <div className="button-group">
-		    <button className="btn btn-default" onClick={this.props.onNavigateHome}>
-		      <span className="icon icon-home"></span>
-		    </button>
-		  </div>
-		</div>
-	      </header>
+	      <PostNavbar postPath={postPath} onNavigateHome={onNavigateHome} />
 	      <div className="window-content">
 		<PostView postPath={postPath} isLoaded={isLoaded} post={post} />
 	      </div>
