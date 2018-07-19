1e8060554e613fda8a4de6b7bd281ef9e47285a6
Pascal Bugnion <pascal@bugnion.org>
2018-07-19 15:52:51 +0100

    Refactor finding tags into separate method

--- app/services/posts.js
+++ app/services/posts.js
@@ -1,12 +1,17 @@
 
 export class Tags {
+    static findAll(filePath, post) {
+	const tags = post.tags ? post.tags : []
+	if (! Array.isArray(tags)) {
+	    throw new Error(`Tags for ${filePath} is not an array.`)
+	}
+	return tags
+    }
+
     static buildTagInvertedIndex(posts) {
 	const tagsIndex = {}
 	Object.entries(posts).map(([filePath, post]) => {
-	    const tags = post.tags ? post.tags : []
-	    if (! Array.isArray(tags)) {
-		throw new Error(`Tags for ${filePath} is not an array.`)
-	    }
+	    const tags = this.findAll(filePath, post)
 	    tags.forEach(tag => {
 		if (typeof tagsIndex[tag] === 'undefined') {
 		    tagsIndex[tag] = [filePath]
