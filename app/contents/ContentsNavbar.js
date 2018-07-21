f7935ad8c3e57f2c7011a227dc90f4fd3d1646dd
Pascal Bugnion <pascal@bugnion.org>
2018-07-21 07:00:46 +0100

    Create navbar component

---
+++ app/contents/ContentsNavbar.js
@@ -0,0 +1,14 @@
+
+import React, { PureComponent } from 'react'
+
+class ContentsNavbar extends PureComponent {
+    render() {
+	return (
+	    <header className="toolbar toolbar-header">
+	      <h1 className="title">Exocortex</h1>
+	    </header>
+	)
+    }
+}
+
+export default ContentsNavbar
