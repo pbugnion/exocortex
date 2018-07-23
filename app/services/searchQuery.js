572f998eda01bb98d13d88f8ef336d4db529234e
Pascal Bugnion <pascal@bugnion.org>
2018-07-23 07:56:44 +0100

    Refactor splitting search string

--- app/services/searchQuery.js
+++ app/services/searchQuery.js
@@ -7,38 +7,45 @@ export class SearchQuery {
 	let currentTerm = ''
 	let isInsideQuotes = false
 	let currentQuoteDelimiter = null
+
+	const pushCurrentTerm = () => {
+	    if (currentTerm.length > 0) {
+		terms.push(currentTerm)
+	    }
+	    currentTerm = ''
+	}
+
+	const startQuotedBlock = delimiter => {
+	    isInsideQuotes = true
+	    currentQuoteDelimiter = delimiter
+	}
+
+	const endQuotedBlock = () => {
+	    isInsideQuotes = false
+	    currentQuoteDelimiter = null
+	}
 	
 	for (let ichar = 0; ichar < query.length; ichar++) {
 	    const char = query.charAt(ichar)
 	    if (!isInsideQuotes) {
 		if (char === ' ') {
-		    if (currentTerm.length > 0) {
-			terms.push(currentTerm)
-		    }
-		    currentTerm = ''
+		    pushCurrentTerm()
 		} else if (quoteTerms.includes(char)) {
-		    isInsideQuotes = true
-		    currentQuoteDelimiter = char
+		    startQuotedBlock(char)
 		} else {
 		    currentTerm += char
 		}
 	    } else {
 		// inside quotes
 		if (char === currentQuoteDelimiter) {
-		    if (currentTerm.length > 0) {
-			terms.push(currentTerm)
-		    }
-		    currentTerm = ''
-		    isInsideQuotes = false
-		    currentQuoteDelimiter = null
+		    pushCurrentTerm()
+		    endQuotedBlock()
 		} else {
 		    currentTerm += char
 		}
 	    }
 	}
-	if (currentTerm.length > 0) {
-	    terms.push(currentTerm)
-	}
+	pushCurrentTerm()
 	return terms
     }
 }
