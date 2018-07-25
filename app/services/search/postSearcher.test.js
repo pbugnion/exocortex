017c7d9346a01de5df707e0d43381c7386e7f0e9
Pascal Bugnion <pascal@bugnion.org>
2018-07-25 07:49:37 +0100

    Tests for multiple terms

--- app/services/search/postSearcher.test.js
+++ app/services/search/postSearcher.test.js
@@ -11,11 +11,33 @@ describe('PostSearcher.searchPost', () => {
 	AstSearcher = require('./astSearcher').AstSearcher
     })
 
-    it.only('single term in AST', () => {
+    it('single term in AST', () => {
 	AstSearcher.buildRelevantAst = jest.fn().mockReturnValue({ relevance: 22 })
-	const post = { ast: 'some ast' }
+	const ast = 'some ast'
+	const post = { ast }
 	expect(PostSearcher.searchPost(post, ['search-term'])).toEqual({ relevance: 22 })
-	expect(AstSearcher.buildRelevantAst).toBeCalledWith(post, 'search-term')
+	expect(AstSearcher.buildRelevantAst).toBeCalledWith(ast, 'search-term')
+    })
+
+    it('multipe terms in AST', () => {
+	AstSearcher.buildRelevantAst = jest.fn()
+	    .mockReturnValueOnce({ relevance: 22 })
+	    .mockReturnValueOnce({ relevance: 2 })
+	const ast = 'some ast'
+	const post = { ast }
+	expect(PostSearcher.searchPost(post, ['first', 'second'])).toEqual({ relevance: 44 })
+	expect(AstSearcher.buildRelevantAst).toBeCalledWith(ast, 'first')
+	expect(AstSearcher.buildRelevantAst).toBeCalledWith(ast, 'second')
+    })
+
+    it('term with no relevance', () => {
+	AstSearcher.buildRelevantAst = jest.fn()
+	    .mockReturnValueOnce({ relevance: 0 })
+	    .mockReturnValueOnce({ relevance: 2 })
+	const ast = 'some ast'
+	const post = { ast }
+	expect(PostSearcher.searchPost(post, ['first', 'second'])).toEqual({ relevance: 0 })
+	expect(AstSearcher.buildRelevantAst).toBeCalledWith(ast, 'first')
     })
 
 })
