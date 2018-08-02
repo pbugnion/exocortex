60e718b1fe8f10b54f07fefd10598efbeff0fc2a
Pascal Bugnion <pascal@bugnion.org>
2018-08-02 10:38:18 +0100

    Syntax highlighting for posts

---
+++ app/posts/CodeBlock.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import Lowlight from 'lowlight';
+import ReactLowlight from 'react-lowlight';
+
+//import scala from 'highlight.js/lib/languages/scala'
+//import python from 'highlight.js/lib/languages/python'
+
+import 'highlight.js/styles/dracula.css'
+
+// Lowlight.registerLanguage('scala', scala)
+// Lowlight.registerLanguage('python', python)
+
+const CodeBlock = ({ language, value, inline }) => (
+    <ReactLowlight
+      language={language || 'js'}
+      value={value || ''}
+      inline={inline}
+    />
+)
+
+export default CodeBlock
--- app/posts/PostAst.js
+++ app/posts/PostAst.js
@@ -5,11 +5,17 @@ import defaultRenderers from 'react-markdown/lib/renderers'
 import astToReact from 'react-markdown/lib/ast-to-react'
 import getDefinitions from 'react-markdown/lib/get-definitions'
 
+import CodeBlock from './CodeBlock'
+
 class PostAst extends Component {
     render() {
 	const { ast } = this.props
 	const renderProps = {
-	    renderers: { ...defaultRenderers, yaml: () => null },
+	    renderers: {
+		...defaultRenderers,
+		code: CodeBlock,
+		yaml: () => null
+	    },
 	    definitions: getDefinitions(ast)
 	}
 	return astToReact(ast, renderProps)
