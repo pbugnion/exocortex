195ab5e37ed33e9dc862e782d4df4ed162906607
Pascal Bugnion <pascal@bugnion.org>
2018-08-03 15:54:37 +0100

    Use summary in post list

--- app/contents/PostSearchItem.js
+++ app/contents/PostSearchItem.js
@@ -1,14 +1,14 @@
 
 import React, { Component } from 'react'
 
-import { Tags, Title } from '../services/posts'
+import { Tags, Title, Summary } from '../services/posts'
 
 import TagList from './TagList'
 
 const PostSearchItem = ({ path, post, navigateToPost }) => {
     const title = Title.findOrFallback(path, post)
     const tags = Tags.findAll(post)
-    const intro = 'hello text hello jfdklsfjlds'
+    const intro = Summary.create(post)
     return (
 	<li className="list-group-item" onClick={() => navigateToPost(path)}>
 	  <h3>{title}</h3>
