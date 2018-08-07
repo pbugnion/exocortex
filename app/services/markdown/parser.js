62e728ca3ef746c5b071743eacff7a5d4a9a4d7b
Pascal Bugnion <pascal@bugnion.org>
2018-08-07 07:47:03 +0100

    Fix indentation of markdown parser

--- app/services/markdown/parser.js
+++ app/services/markdown/parser.js
@@ -4,9 +4,9 @@ import unified from 'unified'
 import parse from 'remark-parse'
 
 export class MarkdownParser {
-    static parse(contents) {
-	const parser = unified().use(parse).use(frontmatter, ['yaml'])
-	const ast = parser.parse(contents)
-	return ast
-    }
+  static parse(contents) {
+    const parser = unified().use(parse).use(frontmatter, ['yaml'])
+    const ast = parser.parse(contents)
+    return ast
+  }
 }
