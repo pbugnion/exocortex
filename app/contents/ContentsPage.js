2aa115d9a0abe25e8b461a4f23feacccee77dfd2
Pascal Bugnion <pascal@bugnion.org>
2018-08-07 07:14:35 +0100

    Reindent contents page

--- app/contents/ContentsPage.js
+++ app/contents/ContentsPage.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react'
 
 import ContentsNavbar from './ContentsNavbar'
@@ -7,35 +6,35 @@ import ContentsBody from './ContentsBody'
 import './ContentsPage.scss'
 
 class ContentsPage extends Component {
-    render() {
-	const {
-	    finishedLoadingPosts,
-	    postPaths,
-	    posts,
-	    selection,
-	    searchTerms,
-	    searchCallbacks,
-	    navigateToPost
-	} = this.props
+  render() {
+    const {
+      finishedLoadingPosts,
+      postPaths,
+      posts,
+      selection,
+      searchTerms,
+      searchCallbacks,
+      navigateToPost
+    } = this.props
 
-	return (
-	    <div className="window ContentsPage">
-	      <ContentsNavbar
-		searchTerms={searchTerms}
-		searchCallbacks={searchCallbacks}
-		/>
-	      <div className="window-content">
-		<ContentsBody
-		  finishedLoadingPosts={finishedLoadingPosts}
-		  postPaths={postPaths}
-		  posts={posts}
-		  selection={selection}
-		  navigateToPost={navigateToPost}
-		  />
-	       </div>
-	    </div>
-	)
-    }
+    return (
+      <div className="window ContentsPage">
+	<ContentsNavbar
+	searchTerms={searchTerms}
+	searchCallbacks={searchCallbacks}
+	/>
+	<div className="window-content">
+	  <ContentsBody
+	    finishedLoadingPosts={finishedLoadingPosts}
+	    postPaths={postPaths}
+	    posts={posts}
+	    selection={selection}
+	    navigateToPost={navigateToPost}
+	  />
+	</div>
+      </div>
+    )
+  }
 }
 
 export default ContentsPage
